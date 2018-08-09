9e970a3830e1d498100819bb876b0a74d71e3480
Andrew Leitsius <leits@quintagroup.com>
2018-08-09 14:45:04 +0300

    Prepare openregistry.concierge 0.3.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
 
-version = '0.2'
+version = '0.3'
 
 requires = [
     'pyyaml',
